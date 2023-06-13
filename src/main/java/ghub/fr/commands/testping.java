4fd6baa7d01914aa3bb3a9eef2286898858014a8
lx78WyY0J5 <9j2k37st@gmail.com>
2023-06-13 11:05:38 +0200

    update for test

--- src/main/java/ghub/fr/commands/testping.java
+++ src/main/java/ghub/fr/commands/testping.java
@@ -1,8 +1,15 @@
 package ghub.fr.commands;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.javacord.api.entity.user.User;
 import org.javacord.api.interaction.SlashCommandInteraction;
 import org.javacord.api.interaction.callback.InteractionImmediateResponseBuilder;
 
+import ghub.fr.api.FileSystem;
+import ghub.fr.main.IDs;
 import ghub.fr.main.main;
 
 public class testping {
@@ -17,6 +24,27 @@ public class testping {
                 interactionImmediateResponseBuilder.respond();
                 if (slashCommandInteraction.getUser().isBotOwner()) {
                     try {
+                        for (User user : main.api.getServerById(IDs.serverID).get().getMembers()) {
+                            if (!user.isBot()) {
+                                slashCommandInteraction.getChannel().get()
+                                        .sendMessage(user.getDiscriminatedName())
+                                        .get();
+                                File file = FileSystem.file(user);
+                                if (file == null) {
+                                    slashCommandInteraction.getChannel().get()
+                                            .sendMessage("null")
+                                            .get();
+                                } else {
+                                    FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
+
+                                    slashCommandInteraction.getChannel().get()
+                                            .sendMessage("" +
+                                                    fileConfiguration.get("Gold"))
+                                            .get();
+                                }
+                            }
+                        }
+
                         slashCommandInteraction.getChannel().get()
                                 .sendMessage(
                                         "ghub.fr (Soon?)" + slashCommandInteraction.getChannel().get().getType())
