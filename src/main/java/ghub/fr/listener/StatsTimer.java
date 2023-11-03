b64c442b34b6705f34df7eb2c35b66172c35166f
lx78WyY0J5 <9j2k37st@gmail.com>
2023-11-03 01:19:15 +0100

    fix counter

--- src/main/java/ghub/fr/listener/StatsTimer.java
+++ src/main/java/ghub/fr/listener/StatsTimer.java
@@ -86,7 +86,7 @@ public class StatsTimer {
                             .asServerTextChannel().get().getMessages(1).get().getOldestMessage().get()
                             .edit(embedBuilder);
 
-                    setCounterOnCategory(everyone.getUsers().size(), (Online + DND + IDLE));
+                    setCounterOnCategory((Online + DND + IDLE + Offline), (Online + DND + IDLE));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
